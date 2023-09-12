5cbfd7d564143a7fe55e7c8e7c394f8503acb9e2
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-12 21:35:04 +0100

    Fix square width & height values

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -21,13 +21,13 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Square instance string format"""
+        """ Square instance string format """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
