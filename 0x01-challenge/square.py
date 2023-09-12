ed04dc35987ac4c48844eb9e1695e9551c787308
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-12 21:26:57 +0100

    Fix square documentations

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Defines a Square class """
 
 
 class square():
-
+    """ Documentation """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initialisation of square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +17,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of a square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Square instance string format"""
         return "{}/{}".format(self.width, self.height)
 
 
